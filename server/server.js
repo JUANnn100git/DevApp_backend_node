ba4061548f5cbdec0b2cfd0c445c4dc47ee11844
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-03-11 11:25:49 -0500

    Peticiones HTTP - GET - POST - PUT - DELETE

--- server/server.js
+++ server/server.js
@@ -1,10 +1,24 @@
+require('./config/config');
+
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res) {
-    res.send('Probando Servicio Server');
+app.get('/paises', function(req, res) {
+    res.json('Get Paises');
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando puerto: ', 3000);
+app.post('/paises', function(req, res) {
+    res.json('Post Paises');
+});
+
+app.put('/paises', function(req, res) {
+    res.json('Put Paises');
+});
+
+app.delete('/paises', function(req, res) {
+    res.json('Delete Paises');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
 });
