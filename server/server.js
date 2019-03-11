4a2d84eca06148d4a8b8e8d6fdbdf8332bc9cd70
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-03-11 12:21:02 -0500

    Routes - Ordenando rutas http

--- server/server.js
+++ server/server.js
@@ -5,21 +5,8 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/paises', function(req, res) {
-    res.json('Get Paises Local');
-});
+app.use(require('./routes/paises'));
 
-app.post('/paises', function(req, res) {
-    res.json('Post Paises');
-});
-
-app.put('/paises', function(req, res) {
-    res.json('Put Paises');
-});
-
-app.delete('/paises', function(req, res) {
-    res.json('Delete Paises');
-});
 
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
