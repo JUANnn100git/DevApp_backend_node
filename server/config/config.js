bd16dd0e360387f43bdf3e67916854612eec956f
JUANnn100 <jvavillanuevaaguinaga@gmail.com>
2019-03-11 12:14:41 -0500

    Mongoose - Base de Datos ONLINE

--- server/config/config.js
+++ server/config/config.js
@@ -7,3 +7,16 @@ process.env.PORT = process.env.PORT || 3000;
 //     Entorno
 // ===============================
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============================
+//     Base de Datos
+// ===============================
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/devapp_db';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 require('./config/config');
 
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 
 app.get('/paises', function(req, res) {
-    res.json('Get Paises');
+    res.json('Get Paises Local');
 });
 
 app.post('/paises', function(req, res) {
@@ -18,6 +20,15 @@ app.put('/paises', function(req, res) {
 app.delete('/paises', function(req, res) {
     res.json('Delete Paises');
 });
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
+
+    if (err) throw err;
+
+    console.log('Base de Datos ONLINE');
+
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando puerto: ', process.env.PORT);
